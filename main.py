50d44fd7c95ea1a7cf654615f1fbf9c2b039db08
jamnihil <jamcbritop@gmail.com>
2025-10-04 18:26:25 -0400

    Added the function that calls the GitHub API and received the EventType dictionary.

---
+++ main.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+def call_github_api(username: str) -> tuple[dict, dict]:
+    """
+    Calls the GitHub API with the username provided and sends the data to
+    function [function] to process and filter the type events
+    :param: Username
+    :return: A tuple containing two dictionaries ???
+    """
+    with urllib.request.urlopen(
+            f'https://api.github.com/users/{username}/events',
+            timeout=10
+    ) as response:
+        github_data = response.read()
+
+    print(github_data)
+
+if __name__ == '__main__':
+    call_github_api('juspay')
